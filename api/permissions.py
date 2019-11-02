e03606b00679b6d64f49047c733467f5a44179ce
Tom <tom.rix@munol.org>
2019-11-02 13:25:03 +0000

    Read only permission for unauthenticated users

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 import copy
 
-class MUNOLDjangoModelPermission(DjangoModelPermissions):
+class MUNOLDjangoModelPermission(DjangoModelPermissionsOrAnonReadOnly):
     '''
     Using the DjangoModelPermissions extended by the view action.
     This allows to set the permissions per User or Group in the admin interface manually.
@@ -14,3 +14,19 @@ class MUNOLDjangoModelPermission(DjangoModelPermissions):
     def __init__(self):
         self.perms_map = copy.deepcopy(self.perms_map)
         self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class MUNOLDjangoModelPermissionsOrAnonReadOnly(DjangoModelPermissionsOrAnonReadOnly):
+    '''
+    Using the DjangoModelPermissionsOrAnonReadOnly extended by the view action.
+    If the user is not authenticated he/she can still access the model data in a read only way.
+    This allows to set the permissions per User or Group in the admin interface manually.
+    One can set permissions for single actions (add, change, delete, view) and for single models.
+    For further reference see: 
+    https://github.com/encode/django-rest-framework/blob/master/rest_framework/permissions.py
+    and
+    https://www.django-rest-framework.org/api-guide/permissions/#djangomodelpermissions
+    '''
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']        
