4f455736e91a291ee4e138ed9ac53d4cd53fedd3
Bjarne Hansen <69861971+BjarneHa@users.noreply.github.com>
2025-04-17 14:48:16 +0200

    Add login problem endpoint

--- api/mobile_app/urls.py
+++ api/mobile_app/urls.py
@@ -5,5 +5,6 @@ from api.mobile_app.views import *
 urlpatterns = [
     path('request_code/', RequestLoginCodeView.as_view()),
     path('login/', LoginView.as_view()),
-    path('verify/', VerifyView.as_view())
+    path('verify/', VerifyView.as_view()),
+    path('login_problem/', LoginProblemView.as_view())
 ]
